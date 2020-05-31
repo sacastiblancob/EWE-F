b763974435529ae0911d2eae1b86957d5ce774f8
sacastiblancob <sacastiblancobunal.edu.co>
2020-05-30 21:24:33 -0500

    First branch

--- ecosim1-f/src/readSpatialDistribution_io.F90
+++ ecosim1-f/src/readSpatialDistribution_io.F90
@@ -5,45 +5,62 @@
 ! Istituto Nazionale di Oceanografia e di Geofisica Sperimentale (OGS),
 ! Trieste/Italy.
 !
-! This program is free software; you can redistribute it and/or modify 
-! it under the terms of the GNU General Public License version 2 as 
+! This program is free software; you can redistribute it and/or modify
+! it under the terms of the GNU General Public License version 2 as
 ! published by the Free Software Foundation.
 !
-! This program is distributed in the hope that it will be useful, but 
-! WITHOUT ANY WARRANTY; without even the implied warranty of 
-! MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU 
+! This program is distributed in the hope that it will be useful, but
+! WITHOUT ANY WARRANTY; without even the implied warranty of
+! MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
 ! General Public License for more details.
 !
-! You should have received a copy of the GNU General Public License 
-! along with  this program; if not, 
+! You should have received a copy of the GNU General Public License
+! along with  this program; if not,
 ! see <http://www.gnu.org/licenses/gpl-2.0.html>.
 !========================================================================
 
 subroutine readSpatialDistribution_io ()
 
-  use statevartypesecosim, only: nvars
+  use statevartypesecosim, only: GroupInfo_fname, nvars
   use statevartypesecospace
 
   implicit none
 
   integer :: i, j
-  character(len = 30) :: fileinptr
+  integer              :: nrows        ! number of rows
+  character(len = 250) :: fileinptr
+  character ( len = 250 ) :: dummy
 
-  nvars = 22;
+! added by Sergio to solve hardcoded 22 for nvars
+! open Ecosim data file
+  open(6666, file = GroupInfo_fname, form = 'formatted')
+
+! read number of rows
+  read(6666, *) dummy, nrows
+  close(6666)
+
+! number of state variables <nvars> is equal to the number of rows
+! <nrows> in the scenario parameters matrix
+  nvars = nrows
+
+! nvars = 22;	!original
+
   allocate(spatialhafs(nlat, nlon, nvars))
 
   do i = 1, nvars
       if (i < 10) then
-          write(fileinptr, '(A20, I1, ".csv")') SpatialDistribution_dirname, i
+          write(fileinptr, '(A, I1, ".csv")') TRIM(SpatialDistribution_dirname), i
+          print *,fileinptr
       else
-          write(fileinptr, '(A20, I2, ".csv")') SpatialDistribution_dirname, i
+          write(fileinptr, '(A, I2, ".csv")') TRIM(SpatialDistribution_dirname), i
+          print *,fileinptr
       end if
-      
+
       print *, fileinptr
       open(4440444, file = fileinptr, form = 'formatted')
       read(4440444, *)
       read(4440444, *)
-      
+
       do j = 1, nlat
           read(4440444, *, end = 1000) spatialhafs(j, :, i)
       end do
